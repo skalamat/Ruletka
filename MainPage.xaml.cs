91cb9f6f5b5498cf19e96b4077d17f7759737d4f
Mateusz Skalski <skalski.mateusz.szymon@gmail.com>
2026-03-19 11:00:20 +0100

    Initial commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Ruletka
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
