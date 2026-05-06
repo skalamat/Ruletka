31e1057ed45cc4372655aa8080c3180a4364743a
Mateusz Skalski <skalski.mateusz.szymon@gmail.com>
2026-05-06 23:59:31 +0200

    poprawki w modelu danych i konfigurowanie bazdy danych

---
+++ Data/RuletkaDb.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ruletka.Data
+{
+    class RuletkaDb : DbContext
+    {
+        public DbSet<Bet>Bets {  get; set; }
+        public DbSet<GameRound> GameRounds { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<User> Users { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlite("Data source=ruletka.db");
+            }
+
+        public void DodajUzytkownika(User user)
+        {
+            Users.Add(user);
+            SaveChanges();
+        }
+    }
+}
