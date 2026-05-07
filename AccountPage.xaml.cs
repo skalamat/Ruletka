5522d5e3cf378237bb69a9e2f4b86ef2d1708553
Mateusz Skalski <skalski.mateusz.szymon@gmail.com>
2026-05-07 12:45:56 +0200

    utworzenie wstępnego interfejsu dla okna logowania i rejestracji oraz usunięcie zawartosci AccountPage

--- AccountPage.xaml.cs
+++ AccountPage.xaml.cs
@@ -6,30 +6,4 @@ public partial class AccountPage : ContentPage
     {
         InitializeComponent();
     }
-
-    private void return_Clicked(object sender, EventArgs e)
-    {
-        Navigation.PopAsync();
-    }
-
-    //HOOVER
-    private void ReturnHoverEntered(object? sender, PointerEventArgs e)
-    {
-        ReturnButton.Background = new SolidColorBrush(Color.FromArgb("#a0a0a0"));
-
-    }
-    private void ReturnHoverExited(object? sender, PointerEventArgs e)
-    {
-        ReturnButton.Background = new SolidColorBrush(Colors.SlateGray);
-    }
-
-    private void registerbutton_Clicked(object sender, EventArgs e)
-    {
-
-    }
-
-    private void logInButton_Clicked(object sender, EventArgs e)
-    {
-
-    }
 }
--- LogInPage.xaml.cs
+++ LogInPage.xaml.cs
@@ -6,4 +6,19 @@ public partial class LogInPage : ContentPage
 	{
 		InitializeComponent();
 	}
+    private void return_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PopAsync();
+    }
+
+    //HOOVER
+    private void ReturnHoverEntered(object? sender, PointerEventArgs e)
+    {
+        ReturnButton.Background = new SolidColorBrush(Color.FromArgb("#a0a0a0"));
+
+    }
+    private void ReturnHoverExited(object? sender, PointerEventArgs e)
+    {
+        ReturnButton.Background = new SolidColorBrush(Colors.SlateGray);
+    }
 }
